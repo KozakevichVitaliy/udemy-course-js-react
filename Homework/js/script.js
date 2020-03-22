023a3c017f37794217c11decc89e0662a6d2cab5
KozakevichVitaliy <samcroc2012@gmail.com>
2020-03-22 14:58:52 +0200

    17_task

---
+++ Homework/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  let overlay = $('.overlay');
+  let modal = $('.modal');
+  let btnTimetable = $('li:odd a.timetable');
+  let btnSelect = $('.main_btn');
+  let btnClose = $('.close');
+
+  function showModal() {
+    $(overlay).fadeToggle('slow');
+    $(modal).slideDown();
+  }
+
+  function closeModal() {
+    $(modal).slideUp();
+    $(overlay).fadeToggle('slow');
+  }
+
+  $(btnTimetable).on('click', showModal);
+  $(btnSelect).on('click', showModal);
+
+  $(btnClose).on('click', closeModal);
+  $(overlay).on('click', closeModal);
+});
